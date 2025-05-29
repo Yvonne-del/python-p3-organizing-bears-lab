1b66c802a70ba729d7cfbcce125d80a205cf27cb
Yvonne <ys359375@gmail.com>
2025-05-29 23:04:16 +0300

    .

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr Chocolate', 5, 'M', 'brown', 'S<a', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'black', 'Kenya', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'brown', 'America', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Seagent Brown', 20, 'M', 'brown', 'Russia', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 25, 'F', 'brown', 'India', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 12, 'M', 'black', 'NY', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'brown', 'America', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'F', 'brown', 'Kenya', 1);
